a3c659be89541f7cfb9f958e19dcae99e3715194
Max <maxbogat717@gmail.com>
2020-11-29 16:23:44 -0800

    Users page updated

--- users.php
+++ users.php
@@ -42,9 +42,186 @@ function insert($flag) {
     }
 }
 
+function listUsers($db) {
 
+    if(isset($_GET["date-input"])){
+
+        $selected_date = $_GET["date-input"];
+
+        $sql_users = "SELECT number_of_posts, username, date
+                        FROM( SELECT COUNT(*) AS number_of_posts, users.username, blog.date, RANK() OVER (ORDER BY number_of_posts DESC) AS rk
+						            FROM blog JOIN users ON users.id = blog.user_id 
+                                    WHERE blog.date = '$selected_date'
+                                    GROUP BY users.username 
+                                    ORDER BY number_of_posts DESC ) t
+                        WHERE rk <= 1";
+
+        $result_users = mysqli_query($db, $sql_users);
+
+        usersHeader("Most actice user/users on <span style='color:#2eec71;'>".$selected_date."<span>");
+        if ($result_users->num_rows > 0) {
+            
+            while($row_user = $result_users->fetch_assoc()) {
+
+                echo "<div class = 'section-user'>";
+                echo    "<br>";
+                echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
+                echo    "<p style='color:white'>Number of blogs posted on <span class='hello'>". $row_user['date'] ."</span>:&nbsp&nbsp". $row_user['number_of_posts'] . "</p>";
+                echo "</div>";
+            } 
+        } else { // display "No users to display" if query result is empty
+            echo "<div class = 'section-user'>";
+            echo "<br><br><p>No blogs were posted on <span class='hello'>".$selected_date."</span></p>";
+            echo "</div>";
+        }
+        
+    
+    }
+    else if (isset($_GET["submit-users"]) && ($_GET["user-1"] != "none") && ($_GET["user-2"] != "none")) {
+        
+        $selected_user_1 = $_GET["user-1"];
+        $selected_user_2 = $_GET["user-2"];
+
+        $sql_toId_1 = "SELECT id FROM users WHERE username = '$selected_user_1';";
+        $result_user_1 = mysqli_query($db, $sql_toId_1);
+        $row_user_1 = $result_user_1->fetch_assoc();
+        $user_id_1 = $row_user_1['id'];
+
+        $sql_toId_2 = "SELECT id FROM users WHERE username = '$selected_user_2';";
+        $result_user_2 = mysqli_query($db, $sql_toId_2);
+        $row_user_2 = $result_user_2->fetch_assoc();
+        $user_id_2 = $row_user_2['id'];
+        
+        $sql_users = "SELECT *
+                        FROM users 
+                        WHERE id IN ( SELECT user_id AS id FROM follows WHERE follower_id = '$user_id_1') 
+                        AND id IN ( SELECT user_id AS id FROM follows WHERE follower_id = '$user_id_2');";
+
+        $result_users = mysqli_query($db, $sql_users);
+        
+        usersHeader("List of users who are followed by <span style='color:#2eec71;'>".$selected_user_1."</span> and <span style='color:#2eec71;'>".$selected_user_2."</span>");
+        if ($result_users->num_rows > 0) {
+            while($row_user = $result_users->fetch_assoc()) {
+
+                echo "<div class = 'section-user'>";
+                echo    "<br>";
+                echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
+                echo "</div>";
+            }
+        } else {
+            echo "<div class = 'section-user'>";
+            echo "<br><br><p>No users are followed by <span style='color:#2eec71;'>".$selected_user_1."</span> and <span style='color:#2eec71;'>".$selected_user_2."</span></p>";
+            echo "</div>";
+        }
+    }
+    else if (isset($_GET["non-active"])) {
+
+        $sql_users = "SELECT * FROM users WHERE id NOT IN( SELECT user_id FROM blog );";
+
+        $result_users = mysqli_query($db, $sql_users);
+        usersHeader("List of users who never posted anything");
+        if ($result_users->num_rows > 0) {
+            while($row_user = $result_users->fetch_assoc()) {
+
+                echo "<div class = 'section-user'>";
+                echo    "<br>";
+                echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
+                echo "</div>";
+            }
+        } else {
+            echo "<div class = 'section-user'>";
+            echo "<br><br><p>No users found</p>";
+            echo "</div>";
+        }
+    }
+    else if (isset($_GET["haters"])) {
+
+        $sql_users = "SELECT id AS user_id, users.* 
+                        FROM users WHERE id IN( SELECT DISTINCT (comment.user_id) 
+                                                FROM comment 
+                                                INNER JOIN blog USING(blog_id) 
+                                                WHERE reaction = 'negative' AND comment.user_id NOT IN( SELECT DISTINCT (comment.user_id) 
+                                                                                                        FROM comment INNER JOIN blog USING(blog_id) 
+                                                                                                        WHERE reaction = 'positive' ) )";
+                                                                                    
+
+        $result_users = mysqli_query($db, $sql_users);
+
+        usersHeader("List of users who only leave negative comments");
+        if ($result_users->num_rows > 0) {
+            while($row_user = $result_users->fetch_assoc()) {
+
+                echo "<div class = 'section-user'>";
+                echo    "<br>";
+                echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
+                echo "</div>";
+            }
+        } else {
+            echo "<div class = 'section-user'>";
+            echo "<br><br><p>No users found</p>";
+            echo "</div>";
+        }
+    }
+    else if (isset($_GET["respected"])) {
+
+        $sql_users = "SELECT DISTINCT(users.id) as user_id, users.* 
+                        FROM users INNER JOIN blog ON blog.user_id = users.id 
+                        WHERE user_id IN (SELECT blog.user_id 
+                                            FROM blog INNER JOIN comment ON comment.blog_id = blog.blog_id 
+                                            WHERE blog.user_id NOT IN (SELECT DISTINCT(comment.user_id) 
+                                                                        FROM comment INNER JOIN blog ON comment.blog_id = blog.blog_id 
+                                                                        WHERE reaction = 'Negative'))";
+                                                                                                
+
+        $result_users = mysqli_query($db, $sql_users);
+
+        usersHeader("List of users who only receive positive comments to their posts");
+        if ($result_users->num_rows > 0) {
+            while($row_user = $result_users->fetch_assoc()) {
+
+                echo "<div class = 'section-user'>";
+                echo    "<br>";
+                echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
+                echo "</div>";
+            }
+        } else {
+            echo "<div class = 'section-user'>";
+            echo "<br><br><p>No users found</p>";
+            echo "</div>";
+        }
+    }
+    else {
+        $sql_users = "SELECT * FROM users";
+        $result_users = mysqli_query($db, $sql_users);
+        
+        usersHeader("List of all users");
+        while($row_user = $result_users->fetch_assoc()) {
+
+            // get user id to search for number of blogs
+            $user_id = $row_user['id'];
+            $sql_blogs = "SELECT COUNT(*) as blog_count FROM blog WHERE user_id = $user_id";
+            $result_blogs = mysqli_query($db, $sql_blogs);
+            $row_blogs = $result_blogs->fetch_assoc();
+
+            echo "<div class = 'section-user'>";
+            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
+            echo    "<p style='color:white'>Number of blogs posted: <span class='hello'>" . $row_blogs['blog_count'] . "</span></p>";
+            echo "</div>";
+        }
+    }
+}
+
+function usersHeader($str) {
+    echo "<div class = 'section-user'>";
+        echo    "<br>";
+        echo    "<p style='color:white;text-align:center;font-size:25px;'>".$str."</p>";
+        echo "</div>";
+}
 
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html>
@@ -117,7 +294,7 @@ function insert($flag) {
                     $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' }).val();
                 } );
             </script>
-            <form>List most active user on this day:  <input type="text" name="date-input" autocomplete="off" id="datepicker" placeholder ="Choose Date" onchange='this.form.submit()'>
+            <form id="date-picker">List most active user on this day:  <input type="text" name="date-input" autocomplete="off" id="datepicker" placeholder ="Choose Date" onchange='this.form.submit()'>
             </form>
         </div>
         <!--  FILTER 1 -->
@@ -125,10 +302,10 @@ function insert($flag) {
         <!--  FILTER 2 -->
         <div class = "column-filter"> <!-- filter menu columns -->
             <p>Followed by these users:</p>
-            <form>
-            <select  name='user-1' id='select-reaction'>;
-                <option value="None">
-                    <?php if(isset($_GET["menu"])){ echo $_GET["menu"];} else { echo "Select User"; } ?>
+            <form class="submit-fliter">
+            <select  name='user-1' id='select-user'>;
+                <option value="none">
+                    <?php if(isset($_GET["menu"])){ echo $_GET["menu"];} else { echo "Select User 1"; } ?>
                 </option>
                 <?php
                     $sql_users = "SELECT * FROM users";
@@ -142,14 +319,11 @@ function insert($flag) {
                             echo "<option  value='".$row_user["username"]."'>".$row_user["username"]."</option>";
                         }
                     }
-                    if(isset($_GET["menu"]) && ($_GET["menu"] != " - None - ")){
-                        echo "<option value=' - None - '> - None - </option>";
-                    }
                 ?>
             </select>
-            <select  name='user-2' id='select-reaction'>;
-                <option value="None">
-                    <?php if(isset($_GET["menu"])){ echo $_GET["menu"];} else { echo "Select User"; } ?>
+            <select  name='user-2' id='select-user'>;
+                <option value="none">
+                    <?php if(isset($_GET["menu"])){ echo $_GET["menu"];} else { echo "Select User 2"; } ?>
                 </option>
                 <?php
                     $sql_users = "SELECT * FROM users";
@@ -163,11 +337,8 @@ function insert($flag) {
                             echo "<option  value='".$row_user["username"]."'>".$row_user["username"]."</option>";
                         }
                     }
-                    if(isset($_GET["menu"]) && ($_GET["menu"] != " - None - ")){
-                        echo "<option value=' - None - '> - None - </option>";
-                    }
                 ?>
-            </select>
+            </select> <br>
             <input class="form-input" type="submit" name="submit-users" onchange='this.form.submit()' /></form>
         </div>
         <!--  FILTER 2 -->
@@ -175,19 +346,23 @@ function insert($flag) {
         <!--  FILTER 3 -->
         <div class = "column-filter"> <!-- filter menu columns -->
             <p>List non active users</p>
-            <form><input class="form-input" type="submit" name="non-active" onchange='this.form.submit()' /></form>
+            <form class="submit-fliter"><input class="form-input" type="submit" name="non-active" onchange='this.form.submit()' /></form>
         </div>
         <!--  FILTER 3 -->  
 
+        <!--  FILTER 4 -->  
         <div class = "column-filter"> <!-- filter menu columns -->
             <p>List Haters</p>
-            <form><input class="form-input" type="submit" name="haters" onchange='this.form.submit()' /></form>
+            <form class="submit-fliter"><input class="form-input" type="submit" name="haters" onchange='this.form.submit()' /></form>
         </div>
-
+        <!--  FILTER 4 --> 
+        
+        <!--  FILTER 5 -->  
         <div class = "column-filter"> <!-- filter menu columns -->
             <p>List Respected Users</p>
-            <form><input class="form-input" type="submit" name="respected" onchange='this.form.submit()' /></form>
+            <form class="submit-fliter"><input class="form-input" type="submit" name="respected" onchange='this.form.submit()' /></form>
         </div>
+        <!--  FILTER 5 -->  
 
         <div class ="column-filter">
             <?php
@@ -198,134 +373,8 @@ function insert($flag) {
 </div> <!-- end filter-menu div -->
 <hr>
 <div id = "user-section">
-    
-    <?php
-
-    if(isset($_GET["date-input"])){
-
-        $selected_date = $_GET["date-input"];
-
-        $sql_users = "SELECT number_of_posts, username, date
-                        FROM( SELECT COUNT(*) AS number_of_posts, users.username, blog.date, RANK() OVER (ORDER BY number_of_posts DESC) AS rk
-						            FROM blog JOIN users ON users.id = blog.user_id 
-                                    WHERE blog.date = '$selected_date'
-                                    GROUP BY users.username 
-                                    ORDER BY number_of_posts DESC ) t
-                        WHERE rk <= 1";
-
-        $result_users = mysqli_query($db, $sql_users);
-        while($row_user = $result_users->fetch_assoc()) {
-
-            echo "<div class = 'section-user'>";
-            echo    "<br>";
-            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
-            echo    "<p style='color:white'>Number of blogs posted on ". $row_user['date'] .": <span class='hello'>" . $row_user['number_of_posts'] . "</span></p>";
-            echo "</div>";
-        }
-    
-    }
-    else if (isset($_GET["submit-users"])) {
-        
-        $selected_user_1 = $_GET["user-1"];
-        $selected_user_2 = $_GET["user-2"];
-
-        $sql_toId_1 = "SELECT id FROM users WHERE username = '$selected_user_1';";
-        $result_user_1 = mysqli_query($db, $sql_toId_1);
-        $row_user_1 = $result_user_1->fetch_assoc();
-        $user_id_1 = $row_user_1['id'];
-
-        $sql_toId_2 = "SELECT id FROM users WHERE username = '$selected_user_2';";
-        $result_user_2 = mysqli_query($db, $sql_toId_2);
-        $row_user_2 = $result_user_2->fetch_assoc();
-        $user_id_2 = $row_user_2['id'];
-        
-        echo $row_user_2['id'];
-
-        $sql_users = "SELECT *
-                        FROM users 
-                        WHERE id IN ( SELECT user_id AS id FROM follows WHERE follower_id = '$user_id_1') 
-                        AND id IN ( SELECT user_id AS id FROM follows WHERE follower_id = '$user_id_2');";
-
-        $result_users = mysqli_query($db, $sql_users);
-        while($row_user = $result_users->fetch_assoc()) {
-
-            echo "<div class = 'section-user'>";
-            echo    "<br>";
-            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
-            echo "</div>";
-        }
-    }
-    else if (isset($_GET["non-active"])) {
-
-        $sql_users = "SELECT * FROM users WHERE id NOT IN( SELECT user_id FROM blog );";
-
-        $result_users = mysqli_query($db, $sql_users);
-        while($row_user = $result_users->fetch_assoc()) {
-
-            echo "<div class = 'section-user'>";
-            echo    "<br>";
-            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
-            echo "</div>";
-        }
-    }
-    else if (isset($_GET["haters"])) {
-
-        $sql_users = "SELECT id AS user_id, users.* 
-                        FROM users WHERE id IN( SELECT DISTINCT (comment.user_id) 
-                                                FROM comment 
-                                                INNER JOIN blog USING(blog_id) 
-                                                WHERE reaction = 'negative' AND comment.user_id NOT IN( SELECT DISTINCT (comment.user_id) 
-                                                                                                        FROM comment INNER JOIN blog USING(blog_id) 
-                                                                                                        WHERE reaction = 'positive' ) )";
-                                                                                    
-
-        $result_users = mysqli_query($db, $sql_users);
-        while($row_user = $result_users->fetch_assoc()) {
-
-            echo "<div class = 'section-user'>";
-            echo    "<br>";
-            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
-            echo "</div>";
-        }
-    }
-    else if (isset($_GET["respected"])) {
-
-        $sql_users = "SELECT DISTINCT(users.id) as user_id, users.* 
-                        FROM users INNER JOIN blog ON blog.user_id = users.id 
-                        WHERE user_id IN (SELECT blog.user_id 
-                                            FROM blog INNER JOIN comment ON comment.blog_id = blog.blog_id 
-                                            WHERE blog.user_id NOT IN (SELECT DISTINCT(comment.user_id) 
-                                                                        FROM comment INNER JOIN blog ON comment.blog_id = blog.blog_id 
-                                                                        WHERE reaction = 'Negative'))";
-                                                                                                
-
-        $result_users = mysqli_query($db, $sql_users);
-        while($row_user = $result_users->fetch_assoc()) {
-
-            echo "<div class = 'section-user'>";
-            echo    "<br>";
-            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
-            echo "</div>";
-        }
-    }
-    else {
-        $sql_users = "SELECT * FROM users";
-        $result_users = mysqli_query($db, $sql_users);
-        while($row_user = $result_users->fetch_assoc()) {
-
-            // get user id to search for number of blogs
-            $user_id = $row_user['id'];
-            $sql_blogs = "SELECT COUNT(*) as blog_count FROM blog WHERE user_id = $user_id";
-            $result_blogs = mysqli_query($db, $sql_blogs);
-            $row_blogs = $result_blogs->fetch_assoc();
-
-            echo "<div class = 'section-user'>";
-            echo    "<br>";
-            echo    "<p style='color:white'>Username: <span class='hello'>" . $row_user['username'] . "</span></p>";
-            echo    "<p style='color:white'>Number of blogs posted: <span class='hello'>" . $row_blogs['blog_count'] . "</span></p>";
-            echo "</div>";
-        }
-    }
+    <?php    
+        listUsers($db);    
     ?>
 </div>
 </body>
